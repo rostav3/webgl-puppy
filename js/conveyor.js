52e493d763fd39023b60faa960aca13cee7934f1
Rina Oksman <rinaok@gmail.com>
2018-09-13 23:39:38 +0300

    Default Changelist

--- js/conveyor.js
+++ js/conveyor.js
@@ -6,7 +6,7 @@ function startConveyor(){
 
 function drawBoneOnConveyor() {
     bones.push(drawBone());
-    var nextBone = Math.round(Math.random() * 5000);
+    var nextBone = Math.round(Math.random() * 10000);
     setTimeout(drawBoneOnConveyor,nextBone);
 
 }
@@ -15,8 +15,11 @@ function moveBones() {
     for (let i=0; i<bones.length; i++) {
         bones[i].position.x -= 1;
         if(isJump){
-            if(bones[i].position.x >= X_POSITION && bones[i].position.x <= 5){
+            if(bones[i].position.x >= -5 && bones[i].position.x <= 10){
                 inputPercent ++;
+            }
+            else if(bones[i].position.x > 10 && bones[i].position.x <= 20){
+                inputPercent --;
             }
         }
         else{
