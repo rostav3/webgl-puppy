8f52fbaece4b81b205110ed225302f95e31590fb
Rina Oksman <rinaok@gmail.com>
2018-09-14 03:44:27 +0300

    Default Changelist

--- js/bars.js
+++ js/bars.js
@@ -2,11 +2,13 @@ var inputBar = document.getElementById("inputBar");
 var expressionBar = document.getElementById("expressionBar");
 var speechBar = document.getElementById("speechBar");
 var handsBar = document.getElementById("handsBar");
+var header = document.getElementById("header");
 
 var inputPercent = 50;
 var expressionPercent = 50;
 var speechPercent = 50;
 var handsPercent = 50;
+var averagePercent = 50 ;
 
 function updateBars() {
     inputBar.style.width = inputPercent + '%';
@@ -20,4 +22,17 @@ function updateBars() {
 
     handsBar.style.width = handsPercent + '%';
     handsBar.innerHTML = handsPercent + '%';
+
+    changeHeader();
 }
+
+function changeHeader(){
+    averagePercent = (inputPercent + expressionPercent + speechPercent + handsPercent) / 4;
+    if(averagePercent < 30){
+        header.textContent = "Your child is stressful. Please pay attention to him.";
+    }
+    else if(averagePercent > 70){
+        header.textContent = "Your child seems to be happy.";
+    }
+}
+
